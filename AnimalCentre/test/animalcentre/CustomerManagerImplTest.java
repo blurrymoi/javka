9f7e7af0147ed577a55704c6db3999db2c50b03f
drspockfromstarwars <luna.ignea@gmail.com>
2015-03-10 00:33:43 +0100

    10.3
    testy and snarky comments added

--- AnimalCentre/test/animalcentre/CustomerManagerImplTest.java
+++ AnimalCentre/test/animalcentre/CustomerManagerImplTest.java
@@ -7,10 +7,7 @@ package animalcentre;
 
 import java.util.*;
 import java.sql.SQLException;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
